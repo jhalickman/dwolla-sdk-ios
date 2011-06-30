661be990a2113f0051af3d04491a3b956be03ba9
James Armstead <armsteadj1@gmail.com>
2011-06-29 21:06:13 -0500

    Added AuthorizationController

--- DwollaOAuth/DwollaOAuthEngine.h
+++ DwollaOAuth/DwollaOAuthEngine.h
@@ -3,7 +3,7 @@
 //  DwollaOAuth
 //
 //  Created by James Armstead on 6/29/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Dwolla. All rights reserved.
 //
 
 @class DwollaOAuthEngine;
@@ -24,13 +24,14 @@
 
 @interface DwollaOAuthEngine : NSObject {
     id<DwollaOAuthEngineDelegate> engineDelegate;
-    OAConsumer* rdOAuthConsumer;
-    OAToken*    rdOAuthRequestToken;
-    OAToken*    rdOAuthAccessToken;
-    NSString*   rdOAuthVerifier;
-    NSMutableDictionary* rdConnections; 
+    OAConsumer* engineOAuthConsumer;
+    OAToken*    engineOAuthRequestToken;
+    OAToken*    engineOAuthAccessToken;
+    NSString*   engineOAuthVerifier;
+    NSMutableDictionary* engineConnections; 
 }
 
 + (id)engineWithConsumerKey:(NSString *)consumerKey consumerSecret:(NSString *)consumerSecret delegate:(id<DwollaOAuthEngineDelegate>)delegate;
+- (id)initWithConsumerKey:(NSString *)consumerKey consumerSecret:(NSString *)consumerSecret delegate:(id<DwollaOAuthEngineDelegate>)delegate;
 
 @end
--- DwollaOAuth/DwollaOAuthEngine.m
+++ DwollaOAuth/DwollaOAuthEngine.m
@@ -3,12 +3,23 @@
 //  DwollaOAuth
 //
 //  Created by James Armstead on 6/29/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Dwolla. All rights reserved.
 //
 
 #import "DwollaOAuthEngine.h"
 
 
 @implementation DwollaOAuthEngine
+    + (id)engineWithConsumerKey:(NSString *)consumerKey consumerSecret:(NSString *)consumerSecret delegate:(id<DwollaOAuthEngineDelegate>)delegate {
+        return [[[self alloc] initWithConsumerKey:consumerKey consumerSecret:consumerSecret delegate:delegate] autorelease];
+    }
 
+    - (id)initWithConsumerKey:(NSString *)consumerKey consumerSecret:(NSString *)consumerSecret delegate:(id<DwollaOAuthEngineDelegate>)delegate {
+        if( self == [super init] ) {
+            engineDelegate = delegate;
+            engineOAuthConsumer = [[OAConsumer alloc] initWithKey:consumerKey secret:consumerSecret];
+            engineConnections = [[NSMutableDictionary alloc] init];
+        }
+        return self;
+    }
 @end
