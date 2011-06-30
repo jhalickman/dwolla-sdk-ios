f1c3b60f79805feb099c273f967fcc3fddfccb49
James Armstead <armsteadj1@gmail.com>
2011-06-29 21:55:20 -0500

    Added needed Authorization files

---
+++ DwollaOAuth/DwollaAuthorizationController.h
@@ -0,0 +1,43 @@
+//
+//  DwollaAuthorizationController.h
+//  DwollaOAuth
+//
+//  Created by James Armstead on 6/29/11.
+//  Copyright 2011 Dwolla. All rights reserved.
+//
+//  Largely inspired by LinkedIn OAuth by Sixten Otto <https://github.com/ResultsDirect/LinkedIn-iPhone>
+//
+
+#import <UIKit/UIKit.h>
+#import "OAuthConsumer.h"
+#import "DwollaOAuthEngine.h"
+
+@class DwollaAuthorizationController;
+
+@protocol DwollaAuthorizationControllerDelegate <NSObject>
+
+@optional
+- (void)dwollaAuthorizationControllerSucceeded:(DwollaAuthorizationController *)controller;
+- (void)dwollaAuthorizationControllerFailed:(DwollaAuthorizationController *)controller;
+- (void)dwollaAuthorizationControllerCanceled:(DwollaAuthorizationController *)controller;
+
+@end
+
+
+@interface DwollaAuthorizationController : UIViewController {
+    id<DwollaAuthorizationControllerDelegate> authorizationDelegate;
+    DwollaOAuthEngine* dwollaEngine;
+	UINavigationBar*  authorizationNavBar;
+    UIWebView*        authorizationWebView;
+}
+
+@property (nonatomic, assign)   id<DwollaAuthorizationControllerDelegate> delegate;
+@property (nonatomic, readonly) DwollaOAuthEngine* engine;
+@property (nonatomic, readonly) UINavigationBar* navigationBar;
+
++ (id)authorizationControllerWithEngine:(DwollaOAuthEngine *)engine delegate:(id<DwollaAuthorizationControllerDelegate>)delegate;
+
+- (id)initWithEngine:(DwollaOAuthEngine *)engine delegate:(id<DwollaAuthorizationControllerDelegate>)delegate;
+- (id)initWithConsumerKey:(NSString *)consumerKey consumerSecret:(NSString *)consumerSecret delegate:(id<DwollaAuthorizationControllerDelegate>)delegate;
+
+@end
---
+++ DwollaOAuth/DwollaAuthorizationController.m
@@ -0,0 +1,74 @@
+//
+//  DwollaAuthorizationController.m
+//  DwollaOAuth
+//
+//  Created by James Armstead on 6/29/11.
+//  Copyright 2011 Dwolla. All rights reserved.
+//
+//  Largely inspired by LinkedIn OAuth by Sixten Otto <https://github.com/ResultsDirect/LinkedIn-iPhone>
+//
+
+#import "DwollaAuthorizationController.h"
+
+
+@implementation DwollaAuthorizationController
+
+@synthesize engine = dwollaEngine, delegate = authorizationDelegate, navigationBar = authorizationNavBar;
+
++ (id)authorizationControllerWithEngine:(DwollaOAuthEngine *)engine delegate:(id<DwollaAuthorizationControllerDelegate>)delegate {
+	if( engine.isAuthorized ) return nil;
+	return [[[self alloc] initWithEngine:engine delegate:delegate] autorelease];
+}
+
+- (id)initWithEngine:(DwollaOAuthEngine *)engine delegate:(id<DwollaAuthorizationControllerDelegate>)delegate {
+    if( self == [super initWithNibName:nil bundle:nil] ) {
+        authorizationDelegate = delegate;
+        dwollaEngine = [engine retain];
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didReceiveRequestToken:) name:DwollaEngineRequestTokenNotification object:dwollaEngine];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didReceiveAccessToken:) name:DwollaEngineAccessTokenNotification object:dwollaEngine];
+        
+        [dwollaEngine requestRequestToken];
+    }
+    return self;
+}
+
+- (id)initWithConsumerKey:(NSString *)consumerKey consumerSecret:(NSString *)consumerSecret delegate:(id<DwollaAuthorizationControllerDelegate>)delegate {
+    return [self initWithEngine:[DwollaOAuthEngine engineWithConsumerKey:consumerKey consumerSecret:consumerSecret delegate:nil] delegate:delegate];
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
