fe281130103bca02d0cba5ca4dca8d2043b14ac5
James Armstead <armsteadj1@gmail.com>
2011-06-29 22:03:38 -0500

    Added HTTPURLConnection class

---
+++ DwollaOAuth/DwollaHTTPURLConnection.h
@@ -0,0 +1,16 @@
+//
+//  DwollaHTTPURLConnection.h
+//  DwollaOAuth
+//
+//  Created by James Armstead on 6/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface DwollaHTTPURLConnection : NSObject {
+    
+}
+
+@end
---
+++ DwollaOAuth/DwollaHTTPURLConnection.m
@@ -0,0 +1,14 @@
+//
+//  DwollaHTTPURLConnection.m
+//  DwollaOAuth
+//
+//  Created by James Armstead on 6/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "DwollaHTTPURLConnection.h"
+
+
+@implementation DwollaHTTPURLConnection
+
+@end
