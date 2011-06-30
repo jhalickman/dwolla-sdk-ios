575c66ab3c4a135b56664d0bf54b2ae8996f5a7d
James Armstead <armsteadj1@gmail.com>
2011-06-29 20:57:40 -0500

    Added UIKit include to OATokenManager and adding calls for getting request token

--- DwollaOAuth/DwollaOAuthEngine.h
+++ DwollaOAuth/DwollaOAuthEngine.h
@@ -6,6 +6,8 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
+@class DwollaOAuthEngine;
+
 #import <Foundation/Foundation.h>
 #import "OAuthConsumer.h"
 
@@ -18,13 +20,17 @@
 
 //- (void)dwollaEngine:(DwollaOAuthEngine *)engine requestSucceeded:(RDLinkedInConnectionID *)identifier withResults:(id)results;
 //- (void)dwollaEngine:(DwollaOAuthEngine *)engine requestFailed:(RDLinkedInConnectionID *)identifier withError:(NSError *)error;
-
-@end
-
 @end
 
 @interface DwollaOAuthEngine : NSObject {
-    
+    id<DwollaOAuthEngineDelegate> engineDelegate;
+    OAConsumer* rdOAuthConsumer;
+    OAToken*    rdOAuthRequestToken;
+    OAToken*    rdOAuthAccessToken;
+    NSString*   rdOAuthVerifier;
+    NSMutableDictionary* rdConnections; 
 }
+
++ (id)engineWithConsumerKey:(NSString *)consumerKey consumerSecret:(NSString *)consumerSecret delegate:(id<DwollaOAuthEngineDelegate>)delegate;
 
 @end
